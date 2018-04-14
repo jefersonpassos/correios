5c971b8d1e64e1d8ffa738ede099bdad721b4f3e
rdg <rdg@Jarvis>
2018-04-14 11:03:51 -0300

    ajustes

--- src/main/java/br/senac/tads/pi3b/correios/Pessoa.java
+++ src/main/java/br/senac/tads/pi3b/correios/Pessoa.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.senac.tads.pi3b.agendaweb;
+package br.senac.tads.pi3b.correios;
 
 import java.text.DateFormat;
 import java.text.ParseException;
